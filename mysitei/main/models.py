60d03b0c6f1fde03e0ffe5e52f7a87c1e9bd7b9b
intervice <intervice5@gmail.com>
2025-04-14 14:10:12 +0300

    5 prac

--- mysitei/main/models.py
+++ mysitei/main/models.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 class Category(models.Model):
     category = models.CharField("Категорія", max_length=250, help_text="Максимум 250 символів")
     slug = models.SlugField("Слаг")
+    objects = models.Manager()
 
 
     class Meta:
@@ -15,12 +16,20 @@ class Category(models.Model):
     def __str__(self):
         return self.category
 
+    def get_absolute_url(self):
+        try:
+            url = reverse('articles-category-list', kwargs={"slug": self.slug})
+        except:
+            url = "/"
+        return
+
+
 
 class Article(models.Model):
     title = models.CharField("Заголовок", max_length=250, help_text="Максимум 250 символів")
     description = models.TextField(blank=True, verbose_name="Опис")
     pub_date = models.DateTimeField("Дата публікації", default=timezone.now)
-    slug = models.SlugField("Слаг",unique_for_date="pub_date")
+    slug = models.SlugField("Слаг", unique_for_date="pub_date")
     main_page = models.BooleanField("Головна", default=False, help_text="Показувати")
     category = models.ForeignKey(Category, related_name='news', blank=True,
                                  null=True, verbose_name=u'Категорія', on_delete=models.CASCADE)
@@ -36,9 +45,10 @@ class Article(models.Model):
 
     def get_absolute_url(self):
         try:
-            url = reverse('news-detail', kwargs={'year': self.pub_date.strftime("%Y"),
-                                                 'month': self.pub_date.strftime("%m"),
-                                                 'day': self.pub_date.strftime("%d"), 'slug': self.slug, })
+            url = reverse("beat-detail", kwargs={"year": self.pub_date.strftime("%Y"),
+                                                 "month": self.pub_date.strftime("%m"),
+                                                 "day": self.pub_date.strftime("%d"),
+                                                 "slug": self.slug, })
         except:
             url = "/"
         return url
@@ -59,4 +69,3 @@ class ArticleImage(models.Model):
     @property
     def filename(self):
         return self.image.name.rsplit("/", 1)[-1]
-
--- mysitei/main/views.py
+++ mysitei/main/views.py
@@ -1,4 +1,72 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DateDetailView
+from .models import Article, Category
 
-def index(request):
-    return render(request, 'main/index.html')
+
+class HomePageView(ListView):
+    model = Article
+    template_name = "main/index.html"
+    context_object_name = "categories"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        context['articles'] = Article.objects.filter(main_page=True)[:5]
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        categories = Category.objects.all()
+        return categories
+
+
+class ArticleDetail(DateDetailView):
+    model = Article
+    template_name = "main/article_detail.html"
+    context_object_name = 'item'
+    date_field = 'pub_date'
+    query_pk_and_slug = True
+    month_format = "%m"
+    allow_future = True
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ArticleDetail, self).get_context_data(*args, **kwargs)
+        try:
+            context['images'] = context['item'].images.all()
+        except:
+            pass
+
+        return context
+
+
+class ArticleList(ListView):
+    model = Article
+    template_name = "main/article_list.html"
+    context_object_name = 'items'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ArticleList, self).get_context_data(*args, **kwargs)
+        try:
+            context['category'] = Category.objects.get(slug=self.kwargs.get('slug'))
+        except Exception:
+            context['category'] = None
+
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        articles = Article.objects.all()
+        return articles
+
+
+class ArticleCategoryList(ArticleList):
+    template_name = "main/categories_page.html"
+
+    def get_queryset(self, *args, **kwargs):
+        articles = Article.objects.filter(category_slug_in=[self.kwargs['slug']]).distinct()
+        return articles
+
+
+# def blog_view(request):
+#     return render(request, 'main/blog_page.html')
+#
+#
+# def categories_view(request):
+#     return render(request, 'main/categories_page.html')
